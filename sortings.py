7d8f0f3b1c074148d29ced5cca2018c34bfa5ee2
charanchakravarthula <charan.ch@acsicorp.com>
2021-07-31 12:26:53 +0530

    sortings

---
+++ sortings.py
@@ -0,0 +1,16 @@
+# list sorting
+
+list_var=[1,9,2,10,0]
+# ascending order
+list_var.sort()
+print(list_var)
+# descending order
+list_var.sort(reverse=True)
+print(list_var)
+
+# using inbuilt sorted functions
+
+# ascending order
+print(sorted(list_var))
+#descending order
+print(sorted(list_var,reverse=True))
