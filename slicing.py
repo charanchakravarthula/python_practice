41e501476ebd239f3963fb862fedc7056a057c86
charanchakravarthula <charan.ch@acsicorp.com>
2021-07-31 11:53:46 +0530

    slicing practice

---
+++ slicing.py
@@ -0,0 +1,21 @@
+# list slicing
+
+list_var=[1,2,3,4,5]
+
+# index slicing without step
+
+print(list_var[0:4])
+
+#index slicing with step
+
+print(list_var[0:4:2])
+
+#string slicing
+
+str_var="abcdefgh"
+
+print(str_var[2:6:2])
+
+# tuple slicing
+tup_var=(1,2,3,4,5)
+print(tup_var[0:3])
