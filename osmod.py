8e64bd8336c5dd7fd875f0951d24dbce9b7d4d37
charanchakravarthula <charan.ch@acsicorp.com>
2021-07-31 16:48:34 +0530

    final prep

---
+++ osmod.py
@@ -0,0 +1,7 @@
+import os
+# getting current working directory
+print(os.getcwd())
+# this fun will make single directory
+os.mkdir("./direc")
+# this function creates hierarcial directories specified with / delimeter
+os.makedirs("./direct/subdirect")
