697830ee2cc76f0626db9fbb34f31130ba57aac9
charanchakravarthula <charan.ch@acsicorp.com>
2021-07-31 10:58:40 +0530

    playing with tuples

---
+++ tuples.py
@@ -0,0 +1,16 @@
+# creating tuple
+tup_var=(1,2,3)
+
+# converting a list into tuple
+con_var=tuple([1,2,3,4,5])
+print(con_var)
+
+# deleting a tuple
+
+del tup_var
+
+# getting the index of a value in the tuple
+
+index_var=con_var.index(4)
+
+print(index_var)
