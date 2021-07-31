482581dc66a0f02ae755525bdffd26ba81bc8266
charanchakravarthula <charan.ch@acsicorp.com>
2021-07-31 14:55:32 +0530

    added custom module

---
+++ module.py
@@ -0,0 +1,6 @@
+def summer(x,y):
+    return x+y
+def subber(x,y):
+    return x-y
+def multiplr(x,y):
+    return x*y
---
+++ moduleUtiliser.py
@@ -0,0 +1,12 @@
+# importing the entire module\
+
+import module
+print(module.summer(1,2))
+
+# importing partcular func from a module
+from module import subber
+print(subber(3,2))
+
+# using alias
+import module as mad
+print(mad.multiplr(3,6))
