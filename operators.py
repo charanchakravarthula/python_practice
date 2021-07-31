c80d7e661959d2d9788673aafc4fd4bd56eb125d
charanchakravarthula <charan.ch@acsicorp.com>
2021-07-31 12:47:08 +0530

    playing with operators

---
+++ operators.py
@@ -0,0 +1,55 @@
+# arthematic operators
+
+# addition
+print(1+3)
+# subtraction
+print(1-3)
+# multiplications
+print(1*3)
+#integral divison
+print(6//5)
+# floating point division
+print(6/5)
+# power
+print(2**3)
+# remainder
+print(7%2)
+
+# assignment operators along with short hand
+a=70
+a+=1
+a-=2
+a*=10
+a%=2
+a**=3
+a&=3
+
+
+# membership operators in and not in
+
+l=[1,2,3,4]
+print(6 in l)
+print(6 not in l)
+
+# comparision operators
+a=3
+b=5
+print(a==b)
+print(a<=b)
+print(a>=b)
+print(a>b)
+print(a<b)
+
+# logical operators
+print(a and b)
+print(a or b)
+print(not a)
+# bitqwise operators
+a=3
+b=3
+print(a^b)
+print(a|b)
+print(a&b)
+
+
+
